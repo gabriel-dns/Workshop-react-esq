28025a73ba435d7b84a566ae6465ad2540c1a5c6
Livia Rodrigues <liviaxvy@gmail.com>
2022-04-07 00:50:53 -0300

    third

--- src/componentes/form/form.js
+++ src/componentes/form/form.js
@@ -1,15 +1,9 @@
 import React, { useCallback } from 'react'
-import InputForm from  './inputForm'
 import style from  './module.form.css'
-import assunto from './imagens-form/assunto.svg'
 import correio from './imagens-form/correio.svg'
 import email from './imagens-form/email.svg'
-import insta from './imagens-form/insta.svg'
 import pessoa from './imagens-form/pessoa.svg'
-import whats from './imagens-form/whats.svg'
-import Vector from './imagens-form/Vector.svg';
-import { useForm  } from 'react-hook-form'
-import api from '../../services/api'
+
 
 
 export default function(){
